24e36adc9ee5ce48ede185d4c3c1f83d98087b7a
Skrinkook <skrinkook@gmail.com>
2023-09-27 20:02:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.querySelector(".markRead").addEventListener("click", function(e){
-    console.log("")
+    const divs = document.getElementsByClassName('NotificationUnit');
+    const inactiveArray = [];
+
+    for (let i = 0; i < divs.length; i++) {
+        if (divs[i].classList.contains('inactive')) {
+            inactiveArray.push(1);
+        } else {
+            inactiveArray.push(0);
+        }
+    }
+
+    inactiveArray.forEach(function(element, index) {
+        if (element === 0) {
+            let notificationUnit = document.querySelector(`#notification${index+1}`);
+            notificationUnit.classList.add("inactive");
+            let redCircle = document.getElementsByClassName(`newNotification`);
+            redCircle.style.display = "none";
+            let numberOfNotifications = document.querySelector(`#numberOfNotifications`);
+            console.log("Position of 0: " + (index+1));
+        }
+    });
+
 })
