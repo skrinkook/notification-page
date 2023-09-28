a6829380bf73d467ee42ba4b44e388302361a356
Skrinkook <skrinkook@gmail.com>
2023-09-28 21:58:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector(".markRead").addEventListener("click", function(e){
     const divs = document.getElementsByClassName('NotificationUnit');
-    const inactiveArray = [];
+    let inactiveArray = [];
 
     for (let i = 0; i < divs.length; i++) {
         if (divs[i].classList.contains('inactive')) {
@@ -10,15 +10,25 @@ document.querySelector(".markRead").addEventListener("click", function(e){
         }
     }
 
+    //
     inactiveArray.forEach(function(element, index) {
         if (element === 0) {
+            console.log("Position of 0: " + (index+1));
             let notificationUnit = document.querySelector(`#notification${index+1}`);
             notificationUnit.classList.add("inactive");
-            let redCircle = document.getElementsByClassName(`newNotification`);
-            redCircle.style.display = "none";
-            let numberOfNotifications = document.querySelector(`#numberOfNotifications`);
-            console.log("Position of 0: " + (index+1));
+            let redCircle = document.querySelectorAll(".newNotification");
+            redCircle.forEach(function(element){
+                element.style.display = "none";
+            })
         }
     });
 
+    if (inactiveArray.includes(0)) {
+        inactiveArray = inactiveArray.map(element => (element === 0 ? 1 : element));
+    }
+
+    let notificationNumber = document.querySelector("#numberOfNotifications");
+    let zerosCount = inactiveArray.filter(element => element === 0).length;
+    notificationNumber.textContent = zerosCount;
 })
+
